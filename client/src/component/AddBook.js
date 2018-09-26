1041e6c15a76ecc47c7a192b0b653f2aa679b70e
lemonjai <lappangcheung@gmail.com>
2018-09-26 19:35:06 -0400

    add book client working

--- client/src/component/AddBook.js
+++ client/src/component/AddBook.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { graphql } from 'react-apollo'
+import { graphql, compose } from 'react-apollo'
 
-import { getAuthorsQuery } from '../queries/queries'
+
+import { getAuthorsQuery, addBookMutation } from '../queries/queries'
 
 class AddBook extends Component {
 
@@ -13,7 +14,7 @@ class AddBook extends Component {
     }
 
     displayAuthors(){
-        var data = this.props.data
+        var data = this.props.getAuthorsQuery
         if(data.loading){
             return <option disabled>Loading Authors...</option>
         }else{
@@ -26,6 +27,14 @@ class AddBook extends Component {
     submitForm = (event) => {
         event.preventDefault()
         console.log(this.state)
+        this.props.addBookMutation({
+            variables: {
+                title: this.state.title,
+                description: this.state.description,
+                genre: this.state.genre,
+                authorId: this.state.authorId
+            }
+        })
     }
 
     render() {
@@ -75,4 +84,7 @@ class AddBook extends Component {
     }
 }
 
-export default graphql(getAuthorsQuery)(AddBook)
+export default compose(
+    graphql(getAuthorsQuery, {name: "getAuthorsQuery"}),
+    graphql(addBookMutation, {name: "addBookMutation"})
+)(AddBook)
--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -23,5 +23,17 @@ const getAuthorsQuery = gql `
     }
 `
 
+// Add books
+const addBookMutation = gql`
+    mutation AddBook($title: String!, $genre: String!, $description: String!, $authorId: ID!){
+        addBook(title: $title, genre: $genre, description: $description, authorId: $authorId){
+            id
+            title
+            description
+            genre
+        }
+    }
+`
+
 // Export the methods
-export { getBooksQuery, getAuthorsQuery }
+export { getBooksQuery, getAuthorsQuery, addBookMutation }
